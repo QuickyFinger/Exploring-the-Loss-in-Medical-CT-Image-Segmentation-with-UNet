4c2c2beb395bfe4abbbc1dcc9852916fc4ec0be8
redhand <30518857+QuickyFinger@users.noreply.github.com>
2019-12-12 19:25:50 +0800

    Add files via upload

---
+++ SurfaceUnet.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python2.7
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def convBatch(nin, nout, kernel_size=3, stride=1, padding=1, bias=False, layer=nn.Conv2d, dilation=1):
+    return nn.Sequential(
+        layer(nin, nout, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias, dilation=dilation),
+        nn.BatchNorm2d(nout),
+        nn.PReLU()
+    )
+
+
+def upSampleConv(nin, nout, kernel_size=3, upscale=2, padding=1, bias=False):
+    return nn.Sequential(
+        nn.Upsample(scale_factor=upscale),
+        convBatch(nin, nout, kernel_size=kernel_size, stride=1, padding=padding, bias=bias),
+        convBatch(nout, nout, kernel_size=3, stride=1, padding=1, bias=bias),
+    )
+
+
+class residualConv(nn.Module):
+    def __init__(self, nin, nout):
+        super(residualConv, self).__init__()
+        self.convs = nn.Sequential(
+            convBatch(nin, nout),
+            nn.Conv2d(nout, nout, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(nout)
+        )
+        self.res = nn.Sequential()
+        if nin != nout:
+            self.res = nn.Sequential(
+                nn.Conv2d(nin, nout, kernel_size=1, bias=False),
+                nn.BatchNorm2d(nout)
+            )
+
+    def forward(self, input):
+        out = self.convs(input)
+        return F.leaky_relu(out + self.res(input), 0.2)
+
+def weights_init(m):
+    if type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d:
+        nn.init.xavier_normal_(m.weight.data)
+    elif type(m) == nn.BatchNorm2d:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+
+class Dummy(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+
+        self.down = nn.Conv2d(in_dim, 10, kernel_size=2, stride=2)
+        self.up = nn.ConvTranspose2d(10, out_dim, kernel_size=3, stride=2, padding=1, output_padding=1)
+
+    def forward(self, input):
+        return self.up(self.down(input))
+
+
+Dimwit = Dummy
+
+
+class UNet(nn.Module):
+    def __init__(self, nin, nout, nG=64):
+        super(UNet, self).__init__()
+
+        self.conv0 = nn.Sequential(convBatch(nin, nG),
+                                   convBatch(nG, nG))
+        self.conv1 = nn.Sequential(convBatch(nG * 1, nG * 2, stride=2),
+                                   convBatch(nG * 2, nG * 2))
+        self.conv2 = nn.Sequential(convBatch(nG * 2, nG * 4, stride=2),
+                                   convBatch(nG * 4, nG * 4))
+
+        self.bridge = nn.Sequential(convBatch(nG * 4, nG * 8, stride=2),
+                                    residualConv(nG * 8, nG * 8),
+                                    convBatch(nG * 8, nG * 8))
+
+        self.deconv1 = upSampleConv(nG * 8, nG * 8)
+        self.conv5 = nn.Sequential(convBatch(nG * 12, nG * 4),
+                                   convBatch(nG * 4, nG * 4))
+        self.deconv2 = upSampleConv(nG * 4, nG * 4)
+        self.conv6 = nn.Sequential(convBatch(nG * 6, nG * 2),
+                                   convBatch(nG * 2, nG * 2))
+        self.deconv3 = upSampleConv(nG * 2, nG * 2)
+        self.conv7 = nn.Sequential(convBatch(nG * 3, nG * 1),
+                                   convBatch(nG * 1, nG * 1))
+        self.final = nn.Conv2d(nG, nout, kernel_size=1)
+
+    def forward(self, input):
+        input = input.float()
+        x0 = self.conv0(input)
+        x1 = self.conv1(x0)
+        x2 = self.conv2(x1)
+
+        bridge = self.bridge(x2)
+
+        y0 = self.deconv1(bridge)
+        y1 = self.deconv2(self.conv5(torch.cat((y0, x2), dim=1)))
+        y2 = self.deconv3(self.conv6(torch.cat((y1, x1), dim=1)))
+        y3 = self.conv7(torch.cat((y2, x0), dim=1))
+
+        return self.final(y3)
